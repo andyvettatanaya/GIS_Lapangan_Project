214ad971ecc782dfd32a9ca38885f726feb88638
Andyvettatanaya <Andyvettatanaya@users.noreply.github.com>
2021-06-10 11:06:22 +0800

    Upload File Koneksi

---
+++ Koneksi webhost/rumahsakitjson.php
@@ -0,0 +1,27 @@
+<?php
+    $server = "localhost";
+    $username = "id16992561_lapangan";
+    $password = "d{f7nIIMruqA(RjV";
+    $dbname = "id16992561_lapangangis";
+
+    //Koneksi
+    $conn = new mysqli($server,$username,$password,$dbname);
+
+    //check koneksi
+    if($conn->connect_error){
+        die("Koneksi Gagal / Failed").$conn->connect_error;
+    }
+    $sql = "SELECT * FROM t_rumahsakit";
+    $result = $conn->query($sql);
+    $data = array();
+
+    if($result->num_rows>0){
+        while ($row=$result->fetch_assoc()){
+            $data[]=$row;
+        }
+    }else{
+        echo "Data tidak ditemukan karena masih kosong";
+    }
+    echo json_encode($data);
+    $conn->close();
+?>
